bdf677de809fd2ace3baf19c67ee0f54844ccdd9
Lucas Emanuel <125845662+Lucaspm5@users.noreply.github.com>
2023-05-20 12:50:15 -0300

    Update and rename 9.py to 2408.cpp

---
+++ OBI 2012/2408.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int a[3];
+    int maior = 0, menor = INT_MAX;
+
+    for (int i = 0;i < 3;i++)
+        scanf("%d",&a[i]);
+
+    for (int i = 0;i < 3;i++)
+    {
+        if (a[i] > maior) maior = a[i];
+        if (a[i] < menor) menor = a[i];
+    }
+
+    for (int i = 0;i < 3;i++)
+    {
+        if(a[i] != maior && a[i] != menor) {printf("%d\n",a[i]);break; }
+    }
+
+    return 0;
+}
